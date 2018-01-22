4415c42e870df802159c8591490eac8d560b5a50
Kris Sakarias <ksakarias@gmail.com>
2018-01-22 15:21:36 -0800

    implement selected and displayed counters

--- busmall.js
+++ busmall.js
@@ -8,10 +8,12 @@ var elArray = [imgEl1, imgEl2, imgEl3];
 var images = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
 Item.allItems = [];
 
-function Item(src, name) {
+function Item(src, alt) {
   this.src = src;
-  this.name = name;
+  this.alt = alt;
   Item.allItems.push(this);
+  this.displayed = 0;
+  this.selected = 0;
 }
 
 for (var i = 0; i < images.length; i++) {
@@ -28,23 +30,34 @@ imgEl1.addEventListener('click', randomizer);
 imgEl2.addEventListener('click', randomizer);
 imgEl3.addEventListener('click', randomizer);
 
-function randomizer() {
+function randomizer(e) {
+  // keep track of image that was clicked
+  var target = e.target.alt;
+  for (var x = 0; x < Item.allItems.length; x++) {
+    if (Item.allItems[x].alt === target) {
+      Item.allItems[x].selected += 1;
+      console.log(Item.allItems[x].selected);
+    }
+  }
   var randomNumberArray = [];
+  console.log('Selected: ' + target);
 
+  // generate new set of random images and count the ones displayed
   for (var i = 0; i < elArray.length; i++) {
     var random = Math.floor(Math.random() * Item.allItems.length);
     if (randomNumberArray.includes(random)) {
       i -= 1;
     } else {
       randomNumberArray.push(random);
+      Item.allItems[random].displayed += 1;
+      console.log('Displayed: ' + Item.allItems[random].alt + ' ' + Item.allItems[random].displayed);
     }
   }
-  console.log(randomNumberArray);
 
-  // var randomNumber = Math.floor(Math.random() * Item.allItems.length);
   imgEl1.setAttribute('src', Item.allItems[randomNumberArray[0]].src);
+  imgEl1.alt = Item.allItems[randomNumberArray[0]].alt;
   imgEl2.setAttribute('src', Item.allItems[randomNumberArray[1]].src);
+  imgEl2.alt = Item.allItems[randomNumberArray[1]].alt;
   imgEl3.setAttribute('src', Item.allItems[randomNumberArray[2]].src);
+  imgEl3.alt = Item.allItems[randomNumberArray[2]].alt;
 }
-
-randomizer();
